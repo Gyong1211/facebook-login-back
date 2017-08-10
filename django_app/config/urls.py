627ba8f70eb3103cfc93fb341f9078cb8fe6faca
Gihong Kim <gyong1211@gmail.com>
2017-08-10 16:30:50 +0900

    FacebookLoginAPIView 기본 구조 구현, debug_token을 사용해 입력받은 token이 valid한지 여부 판단

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from member.serializers import FacebookLoginAPIView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^facebook-login/', FacebookLoginAPIView.as_view()),
 ]
